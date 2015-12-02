0bfae5bf9fb2c8cc6195417e78b00511f083a754
nathan-contino <ncontino@u.rochester.edu>
2015-12-02 18:22:35 -0500

    Final Touches for Pres
    I <3 pgbovine

--- registered/auth/forms.py
+++ registered/auth/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class CreateUserForm(forms.Form):
     username = forms.CharField(max_length=200,required=True)
     email = forms.EmailField(max_length=200,required=True)
-    password = forms.CharField(max_length=200,required=True)
+    password = forms.CharField(max_length=200,required=True, widget=forms.PasswordInput())
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=200,required=True)
